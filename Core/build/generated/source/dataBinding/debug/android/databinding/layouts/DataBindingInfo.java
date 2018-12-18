73e51404c86cab204d04a2eeca6ce34dd3494204
jack2018 <610184089@qq.com>
2018-12-19 05:30:19 +0800

    Initial commit

---
+++ Core/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="6bdfb051-2d26-467c-804d-069c170a0ed8")
+public class DataBindingInfo {}
---
+++ Core/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="1839b18c-6adb-4c21-963c-5ca65cad7e23")
+public class DataBindingInfo {}
